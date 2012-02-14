8f1053daee4e5c22dc444ce33468dd96213f9372
Peter Michaux <petermichaux@gmail.com>
2012-02-14 15:24:05 -0800

    add thisObj parameter to subject objects and rework subject significantly

--- src/prototype/subject.js
+++ src/prototype/subject.js
@@ -1,18 +1,17 @@
-// A prototypical subject object.
-//
-var LIB_subject;
+var LIB_Subject = function() {};
 
 (function() {
 
-    var has = function(o, p) {
+    function has(o, p) {
         return Object.prototype.hasOwnProperty.call(o, p);
-    };
+    }
 
-    function removeListener(listeners, listener) {
+    function removeListener(listeners, listener, thisObj) {
         // Loop backwards through the array so adjacent references
         // to "listener" are all removed.
         for (var i = listeners.length; i--; ) {
-            if (listeners[i] === listener) {
+            if ((listeners[i].listener === listener) &&
+                ((!thisObj) || (listeners[i].thisObj === thisObj))) {
                 listeners.splice(i, 1);
             }
         }
@@ -24,150 +23,56 @@ var LIB_subject;
         // iterating over the list.
         listeners = listeners.slice(0);
         for (var i = 0, ilen = listeners.length; i < ilen; i++) {
-            listeners[i](data);
+            listeners[i].listener.call(listeners[i].thisObj, data);
         }
     }
 
-    LIB_subject = {
+    // "event" is an event name string.
+    // "listener" is a callback function.
+    //
+    // One listener can be added multiple times.
+    //
+    LIB_Subject.prototype.addEventListener = function(event, listener, thisObj) {
+        has(this, '_LIB_listeners') || (this._LIB_listeners = {});
+        has(this._LIB_listeners, event) || (this._LIB_listeners[event] = []);
+        this._LIB_listeners[event].push({listener:listener, thisObj:thisObj});
+    };
 
-        // "event" is an event name string.
-        // "listener" is a callback function.
-        //
-        // One listener can be added multiple times.
-        //
-        addEventListener: function(event, listener) {
-            if (listener) {
-                has(this, '_listeners') || (this._listeners = {});
-                has(this._listeners, event) || (this._listeners[event] = []);
-                this._listeners[event].push(listener);
-            }
-            else {
-                // the listener function for all events is in the event variable
-                has(this, '_allListeners') || (this._allListeners = []);
-                this._allListeners.push(event);
-            }
-        },
+    // addEventListener allows one listener to be added multiple times.
+    // We remove all references to "listener".
+    //
+    // No complaints if the "listener" is not found in the list.
+    //
+    LIB_Subject.prototype.removeEventListener = function(event, listener, thisObj) {
+        if (has(this, '_LIB_listeners') &&
+            has(this._LIB_listeners, event)) {
+            removeListener(this._LIB_listeners[event], listener, thisObj);
+        }
+    };
 
-        // addEventListener allows one listener to be added multiple times.
-        // We remove all references to "listener".
-        //
-        // No complaints if the "listener" is not found in the list.
-        //
-        removeEventListener: function(event, listener) {
-            if (listener) {
-                if (has(this, '_listeners') &&
-                    has(this._listeners, event)) {
-                    removeListener(this._listeners[event], listener);
-                }
+    // The "data" will be pushed to each listener.
+    // The "data.type" value is required and must be a string name of an event type.
+    //
+    LIB_Subject.prototype.dispatchEvent = function(data) {
+        if (has(this, '_LIB_listeners')) {
+            if (has(this._LIB_listeners, data.type)) {
+                callListeners(this._LIB_listeners[data.type], data);
             }
-            else {
-                // the listener function for all events is in the event variable
-                if (has(this, '_allListeners')) {
-                    removeListener(this._allListeners, event);
-                }
-            }
-        },
-
-        // The "data" will be pushed to each listener.
-        // The "data.type" value is required and must be a string name of an event type.
-        //
-        dispatchEvent: function(data) {
-            if (has(this, '_listeners') &&
-                has(this._listeners, data.type)) {
-                callListeners(this._listeners[data.type], data);
-            }
-            if (has(this, '_allListeners')) {
-                callListeners(this._allListeners, data);
+            if (has(this._LIB_listeners, 'LIB_all')) {
+                callListeners(this._LIB_listeners.LIB_all, data);
             }
         }
-
     };
 
 }());
 
-
-var LIB_Subject = function() {
-    // The constructor property is unreliable in general
-    // but in case someone is depending on it we repair it.
-    this.constructor = LIB_Subject;
-};
-LIB_Subject.prototype = LIB_subject;
-
-
 var LIB_mixinSubject = function(obj) {
-    for (var p in LIB_subject) {
-        if (Object.prototype.hasOwnProperty.call(LIB_subject, p) &&
-            // Don't want to copy LIB_subject._listeners object. Want the obj object
-            // to have its own listeners and not share listeners with LIB_subject.
-            (typeof LIB_subject[p] === 'function')) {
-            obj[p] = LIB_subject[p];
+    for (var p in LIB_Subject.prototype) {
+        if (Object.prototype.hasOwnProperty.call(LIB_Subject.prototype, p) &&
+            // Don't want to copy LIB_Subject.prototype._LIB_listeners object. Want the obj object
+            // to have its own listeners and not share listeners with LIB_Subject.prototype.
+            (typeof LIB_Subject.prototype[p] === 'function')) {
+            obj[p] = LIB_Subject.prototype[p];
         }
     }
 };
-
-
-// The pub/sub pattern works with the global LIB_subject object
-// because it is a fully-fledged subject object.
-//
-// LIB_subject.addEventListener('foo', function(data) {
-//     console.log(data.tweet);
-// });
-// LIB_subject.addEventListener('bar', function(data) {
-//     console.log(data.tweet);
-// });
-// LIB_subject.dispatchEvent({
-//     type: 'foo', 
-//     tweet: 'Great sandwich!'
-// });
-
-
-// New subject objects can be created.
-//
-// var twitter = new LIB_Subject();
-// twitter.addEventListener('foo', function(data) {
-//     console.log(data.tweet);
-// });
-// twitter.addEventListener('bar', function(data) {
-//     console.log(data.tweet);
-// });
-// twitter.dispatchEvent({
-//     type: 'foo', 
-//     tweet: 'So tired!'
-// });
-
-
-// The subject methods can be mixed into 
-// other objects.
-//
-// function APP_Twitter(name) {
-//     this.name = name;
-// };
-// LIB_mixinSubject(APP_Twitter.prototype);
-//
-// var tweetsRUs = new APP_Twitter('Tweets R Us');
-// tweetsRUs.addEventListener('foo', function(data) {
-//     console.log(data.tweet);
-// });
-// tweetsRUs.addEventListener('bar', function(data) {
-//     console.log(data.tweet);
-// });
-// tweetsRUs.dispatchEvent({
-//     type: 'foo',
-//     tweet: 'Homework sucks!'
-// });
-
-
-// And even the APP_Twitter.prototype object
-// is still a fully functioning subject object
-// with its own listeners.
-// 
-// APP_Twitter.prototype.addEventListener('foo', function(data) {
-//     console.log(data.tweet);
-// });
-// APP_Twitter.prototype.addEventListener('bar', function(data) {
-//     console.log(data.tweet);
-// });
-// APP_Twitter.prototype.dispatchEvent({
-//     type: 'foo', 
-//     tweet: 'No thoughts lately.'
-// });
--- tst/prototype/subjectSuite.js
+++ tst/prototype/subjectSuite.js
@@ -67,20 +67,20 @@ var subjectSuite;
             feedAll = [];
 
             // add some listeners
-            LIB_subject.addEventListener('foo', listener1);
-            LIB_subject.addEventListener('bar', listener1b);
-            LIB_subject.addEventListener(listenerAll);
+            LIB_Subject.prototype.addEventListener('foo', listener1);
+            LIB_Subject.prototype.addEventListener('bar', listener1b);
+            LIB_Subject.prototype.addEventListener('LIB_all', listenerAll);
             twitter.addEventListener('foo', listener2);
             tweetsRUs.addEventListener('foo', listener3);
             APP_Twitter.prototype.addEventListener('foo', listener4);
 
             // send some tweets
-            LIB_subject.dispatchEvent({
+            LIB_Subject.prototype.dispatchEvent({
                 type: 'foo',
                 tweet: tweet1
             });
 
-            LIB_subject.dispatchEvent({
+            LIB_Subject.prototype.dispatchEvent({
                 type: 'bar',
                 tweet: tweet1b
             });
@@ -109,20 +109,20 @@ var subjectSuite;
 
         tearDown: function() {
             // remove the listeners
-            LIB_subject.removeEventListener('foo', listener1);
-            LIB_subject.removeEventListener('bar', listener1b);
-            LIB_subject.removeEventListener(listenerAll);
+            LIB_Subject.prototype.removeEventListener('foo', listener1);
+            LIB_Subject.prototype.removeEventListener('bar', listener1b);
+            LIB_Subject.prototype.removeEventListener('LIB_all', listenerAll);
             twitter.removeEventListener('foo', listener2);
             tweetsRUs.removeEventListener('foo', listener3);
             APP_Twitter.prototype.removeEventListener('foo', listener4);
         },
 
-        "test LIB_subject constructor": function() {
-            jsUnity.assertIdentical(Object, LIB_subject.constructor, "LIB_subject's constructor should be Object.");
+        "test LIB_Subject.prototype constructor": function() {
+            jsUnity.assertIdentical(LIB_Subject, LIB_Subject.prototype.constructor, "LIB_Subject.prototype's constructor should be Object.");
         },
 
         "test LIB_Subject instance's constructor": function() {
-            jsUnity.assertIdentical(Object, LIB_Subject.prototype.constructor, "LIB_Subject.prototype should have Object as its constructor.");
+            jsUnity.assertIdentical(LIB_Subject, LIB_Subject.prototype.constructor, "LIB_Subject.prototype should have Object as its constructor.");
             jsUnity.assertIdentical(LIB_Subject, (new LIB_Subject()).constructor, "an instance of LIB_Subject should have LIB_Subject as its constructor.");
         },
 
@@ -146,9 +146,46 @@ var subjectSuite;
             jsUnity.assertArrayIdentical([tweet3], feed3);
             jsUnity.assertArrayIdentical([tweet4, tweet5], feed4);
         },
-        
+
         "test all listeners": function() {
             jsUnity.assertArrayIdentical([tweet1, tweet1b], feedAll);
+        },
+
+        "test thisObj argument": function() {
+            var s = new LIB_Subject();
+            var obj0 = {
+                name: 'obj0_name',
+                handler: function(ev) {
+                    this.result = this.name;
+                }
+            };
+            var obj1 = {
+                name: 'obj1_name'
+            };
+            s.addEventListener('foo', obj0.handler, obj0);
+            // borrow obj0's handler and use for obj1
+            s.addEventListener('foo', obj0.handler, obj1);
+
+            jsUnity.assertIdentical(undefined, obj0.result);
+            jsUnity.assertIdentical(undefined, obj1.result);
+
+            s.dispatchEvent({type: 'foo'});
+
+            jsUnity.assertIdentical('obj0_name', obj0.result);
+            jsUnity.assertIdentical('obj1_name', obj1.result);
+            
+            delete obj0.result;
+            delete obj1.result;
+
+            jsUnity.assertIdentical(undefined, obj0.result);
+            jsUnity.assertIdentical(undefined, obj1.result);
+
+            s.removeEventListener('foo', obj0.handler, obj1);
+            s.dispatchEvent({type: 'foo'});
+
+            jsUnity.assertIdentical('obj0_name', obj0.result);
+            jsUnity.assertIdentical(undefined, obj1.result);
+
         }
 
     };
