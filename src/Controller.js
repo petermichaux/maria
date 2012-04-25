07a3bcf61debadf8894a28486e6dca55dffa8773
Peter Michaux <petermichaux@gmail.com>
2012-04-25 13:21:29 -0700

    Add a Controller base class inspired by Smalltalk's Controller class.

---
+++ src/Controller.js
@@ -0,0 +1,25 @@
+var LIB_Controller = function() {};
+
+LIB_Controller.prototype.destroy = function() {
+    this._model = null;
+    if (this._view) {
+        this._view.setController(null);
+        this._view = null;
+    }
+};
+
+LIB_Controller.prototype.getModel = function() {
+    return this._model;
+};
+
+LIB_Controller.prototype.setModel = function(model) {
+    this._model = model;
+};
+
+LIB_Controller.prototype.getView = function() {
+    this._view;
+};
+
+LIB_Controller.prototype.setView = function(view) {
+    this._view = view;
+};
