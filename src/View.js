61c731a70c394605033887fac9ef06e101a024b1
Peter Michaux <petermichaux@gmail.com>
2012-04-27 01:13:40 -0700

    Simplify views. Matches Smalltalk more closely. Views are very abstract.

--- src/View.js
+++ src/View.js
@@ -1,28 +1,70 @@
-// a composite view for the composite design pattern
-// used in the view layer of MVC applications
-//
 maria.View = function() {
     maria.Node.call(this);
-    maria.LeafView.apply(this, arguments);
 };
-
-maria.View.prototype = new maria.LeafView();
-maria.View.prototype.constructor = maria.View;
 
 maria.Node.mixin(maria.View.prototype);
 
-maria.View.prototype.insertBefore = function(newChild, oldChild) {
-    maria.Node.prototype.insertBefore.call(this, newChild, oldChild);
-    this.getRootEl().insertBefore(newChild.getRootEl(), (oldChild ? oldChild.getRootEl() : null));
-};
-
-maria.View.prototype.removeChild = function(oldChild) {
-    maria.Node.prototype.removeChild.call(this, oldChild);
-    var oldChildRootEl = oldChild.getRootEl();
-    oldChildRootEl.parentNode.removeChild(oldChildRootEl);
-};
-
 maria.View.prototype.destroy = function() {
-    maria.LeafView.prototype.destroy.call(this);
+    maria.purgeEventListener(this);
+    this._model = null;
+    if (this._controller) {
+        this._controller.destroy();
+        this._controller = null;
+    }
     maria.Node.prototype.destroy.call(this);
+};
+
+maria.View.prototype.render = function() {
+    // to be overridden by concrete view subclasses
+};
+
+maria.View.prototype.update = function() {
+    // to be overridden by concrete view subclasses
+};
+
+maria.View.prototype.getModel = function() {
+    return this._model;
+};
+
+maria.View.prototype.setModel = function(model) {
+    this.setModelAndController(model, this._controller);
+};
+
+maria.View.prototype.getDefaultControllerConstructor = function() {
+    return maria.Controller;
+};
+
+maria.View.prototype.getDefaultController = function() {
+    var constructor = this.getDefaultControllerConstructor();
+    return new constructor();
+};
+
+maria.View.prototype.getController = function() {
+    if (!this._controller) {
+        this.setController(this.getDefaultController());
+    }
+    return this._controller;
+};
+
+maria.View.prototype.setController = function(controller) {
+    this.setModelAndController(this._model, controller);
+};
+
+maria.View.prototype.setModelAndController = function(model, controller) {
+    if (this._model !== model) {
+        if (this._model) {
+            maria.removeEventListener(this._model, 'change', this, 'update');
+        }
+        if (model) {
+            maria.addEventListener(model, 'change', this, 'update');
+        }
+        this._model = model;
+    }
+    if (this._controller !== controller) {
+        if (controller) {
+            controller.setView(this);
+            controller.setModel(model);
+        }
+        this._controller = controller;
+    }
 };
