becf9700448a88ad22462b2e5ff1910733d5d306
Peter Michaux <petermichaux@gmail.com>
2012-05-02 22:34:45 -0700

    make the borrow function globally accessable

--- src/borrowFromLibs.js
+++ src/borrowFromLibs.js
@@ -1,11 +1,10 @@
-(function() {
-    function borrow(source) {
-        for (var p in source) {
-            if (Object.prototype.hasOwnProperty.call(source, p)) {
-                maria[p] = source[p];
-            }
+maria.borrow = function(sink, source) {
+    for (var p in source) {
+        if (Object.prototype.hasOwnProperty.call(source, p)) {
+            sink[p] = source[p];
         }
     }
-    borrow(evento);
-    borrow(hijos);
-}());
+};
+
+maria.borrow(maria, evento);
+maria.borrow(maria, hijos);
