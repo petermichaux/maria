d3a78eb70bdd898f6260bdd315e7ff1edb7838f1
Peter Michaux <petermichaux@gmail.com>
2012-05-06 08:17:49 -0700

    easy to subclass a View too

--- src/View.js
+++ src/View.js
@@ -121,4 +121,10 @@ maria.View.subclass = function(namespace, name, options) {
     if (methods) {
         maria.borrow(prototype, methods);
     }
+    
+    Constructor.subclass = function(namespace, name, options) {
+        options = options || {};
+        options.SuperConstructor = options.SuperConstructor || Constructor;
+        SuperConstructor.subclass(namespace, name, options);
+    };
 };
