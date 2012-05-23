e15caaa0ae150304540cada5c974efd63e44f11c
Peter Michaux <petermichaux@gmail.com>
2012-05-22 19:40:56 -0700

    namespace localStorage key

--- eg/checkit/src/js/bootstrap.js
+++ eg/checkit/src/js/bootstrap.js
@@ -3,11 +3,11 @@ maria.addEventListener(window, 'load', function() {
     loading.parentNode.removeChild(loading);
 
     if ((typeof localStorage === 'object') && (typeof JSON === 'object')) {
-        var store = localStorage.getItem('todos');
+        var store = localStorage.getItem('todos-maria');
         var model = store ? checkit.TodosModel.fromJSON(JSON.parse(store)) :
                             new checkit.TodosModel();
         evento.addEventListener(model, 'change', function() {
-            localStorage.setItem('todos', JSON.stringify(model.toJSON()));
+            localStorage.setItem('todos-maria', JSON.stringify(model.toJSON()));
         });
     }
 
