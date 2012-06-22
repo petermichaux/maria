bab59407eed529d015bfdf524e4a7d09212d6d4c
Peter Michaux <petermichaux@gmail.com>
2012-06-21 21:35:38 -0700

    SetView tests

--- tst/SetView.subclass-suite.js
+++ tst/SetView.subclass-suite.js
@@ -2,8 +2,8 @@
 
     buster.testCase('SetView.subclass Suite', {
 
-        "test trival": function() {
-            assert(true);
+        "test SetView.subclass is the same as ElementView.subclass": function() {
+            assert.same(maria.ElementView.subclass, maria.SetView.subclass);
         }
 
     });
