68961fc8387c5919c9389750df5c1b696f75efb5
Peter Michaux <petermichaux@gmail.com>
2012-06-21 21:56:13 -0700

    documentation for SetView

--- src/SetView.js
+++ src/SetView.js
@@ -1,3 +1,53 @@
+/**
+
+@property maria.SetView
+
+@parameter model {Object} Optional
+
+@parameter controller {Object} Optional
+
+@parameter document {Document} Optional
+
+@description
+
+A constructor function to create new set view objects.
+
+    var setView = new maria.SetView();
+
+maria.SetView inherits from maria.ElementView and the documentation of
+maria.ElementView will tell you most of what you need to know when
+working with a maria.SetView.
+
+A maria.SetView is intended to be a view for a maria.SetModel. The set
+view will take care child views when elements are added or deleted from
+the set model.
+
+When an element is added to the set, the set view need to know what
+kind of view to make. Your application will redefine or likely override
+the set view's createChildView method.
+
+    maria.SetView.prototype.createChildView = function(model) {
+        return new maria.ElementView(model);
+    };
+
+A particularly useful pattern is using maria.SetView as the
+"superclass" of your application's set views. The following example
+shows how this can be done at a low level for a to-do application. See
+maria.SetView.subclass for a more compact way to accomplish the same.
+
+    checkit.TodosListView = function() {
+        maria.SetView.apply(this, arguments);
+    };
+    checkit.TodosListView.prototype = new maria.SetView();
+    checkit.TodosListView.prototype.constructor = checkit.TodosListView;
+    checkit.TodosListView.prototype.getTemplate = function() {
+        return checkit.TodosListTemplate;
+    };
+    checkit.TodosListView.prototype.createChildView = function(todoModel) {
+        return new checkit.TodoView(todoModel);
+    };
+
+*/
 maria.SetView = function() {
     maria.ElementView.apply(this, arguments);
 };
