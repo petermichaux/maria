78e327da9108604c4a0d0cdf7a0beff05a33e04b
Peter Michaux <petermichaux@gmail.com>
2012-06-21 21:03:55 -0700

    add View.subclass docs

--- src/View.subclass.js
+++ src/View.subclass.js
@@ -1,3 +1,51 @@
+/**
+
+@property maria.View.subclass
+
+@description
+
+A function that makes subclassing maria.View more compact.
+
+The following example creates a myapp.MyView constructor function
+equivalent to the more verbose example shown in the documentation
+for maria.View.
+
+    maria.View.subclass(myapp, 'MyView', {
+        modelActions: {
+            'squashed': 'onSquashed',
+            'squished': 'onSquished'
+        },
+        properties: {
+            initialize: function() {
+                alert('a new view created');
+            },
+            destroy: function() {
+                alert('a view destroyed');
+                maria.View.prototype.destroy.call(this);
+            }
+        }
+    });
+
+This subclassing function implements options following the
+"convention over configuration" philosophy. The myapp.MyView will,
+by convention, use the myapp.MyController constructor. This can be
+configured.
+
+    maria.View.subclass(myapp, 'MyView', {
+        controllerConstructor: myapp.MyController,
+        modelActions: {
+        ...
+
+Alternately you can use late binding by supplying string names of
+objects in the application's namespace object (i.e. the myapp object
+in this example).
+
+    maria.View.subclass(myapp, 'MyView', {
+        controllerConstructorName: 'MyController',
+        modelActions: {
+        ...
+
+*/
 maria.View.subclass = function(namespace, name, options) {
     options = options || {};
     var modelConstructor = options.modelConstructor;
