b7aeb88a64ce8d3e9f8d5b376f22b6a1709f1fc5
Peter Michaux <petermichaux@gmail.com>
2012-06-21 21:51:40 -0700

    a documentation example for SetView.subclass

--- src/SetView.subclass.js
+++ src/SetView.subclass.js
@@ -6,5 +6,20 @@
 
 The same as maria.ElementView.
 
+You will likely want to specify a createChildView method.
+
+The following example creates a checkit.TodosListView constructor
+function equivalent to the more verbose example shown in the
+documentation for maria.SetView.
+
+    maria.SetView.subclass(checkit, 'TodosListView', {
+        modelConstructor: checkit.TodosModel,
+        properties: {
+            createChildView: function(todoModel) {
+                return new checkit.TodoView(todoModel);
+            }
+        }
+    });
+
 */
 maria.SetView.subclass = maria.ElementView.subclass;
