2a8309e8378fca3122bc7606c5891dca669812a3
Peter Michaux <petermichaux@gmail.com>
2012-07-07 08:28:51 -0700

    make examples more realistic while still retaining easy development of Maria and using the examples as part of the testing process of Maria without requiring a build step to work on Maria.

---
+++ eg/checkit/lib/maria/maria.js
@@ -0,0 +1,32 @@
+// The contents of this file would normally be the built maria.js file.
+// To make development of Maria easier, we use document.write so that
+// the examples are always using the latest Maria code and we don't
+// need a build step while developing Maria.
+
+document.write('<script src="../../../lib/evento/evento.js"></script>');
+document.write('<script src="../../../lib/hijos/hijos.js"></script>');
+document.write('<script src="../../../lib/arbutus/arbutus.js"></script>');
+document.write('<script src="../../../lib/grail/grail.js"></script>');
+document.write('<script src="../../../lib/hormigas/hormigas.js"></script>');
+
+document.write('<script src="../../../src/namespace.js"></script>');
+document.write('<script src="../../../src/borrow.js"></script>');
+document.write('<script src="../../../src/borrowEvento.js"></script>');
+document.write('<script src="../../../src/borrowHijos.js"></script>');
+document.write('<script src="../../../src/borrowArbutus.js"></script>');
+document.write('<script src="../../../src/borrowGrail.js"></script>');
+document.write('<script src="../../../src/borrowHormigas.js"></script>');
+document.write('<script src="../../../src/subclass.js"></script>');
+document.write('<script src="../../../src/Model.js"></script>');
+document.write('<script src="../../../src/SetModel.js"></script>');
+document.write('<script src="../../../src/View.js"></script>');
+document.write('<script src="../../../src/ElementView.js"></script>');
+document.write('<script src="../../../src/SetView.js"></script>');
+document.write('<script src="../../../src/Controller.js"></script>');
+
+document.write('<script src="../../../src/Model.subclass.js"></script>');
+document.write('<script src="../../../src/SetModel.subclass.js"></script>');
+document.write('<script src="../../../src/View.subclass.js"></script>');
+document.write('<script src="../../../src/ElementView.subclass.js"></script>');
+document.write('<script src="../../../src/SetView.subclass.js"></script>');
+document.write('<script src="../../../src/Controller.subclass.js"></script>');
