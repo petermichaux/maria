d4c3183853cfffd67ec95b3ff6d09f0eb02e602d
Peter Michaux <petermichaux@gmail.com>
2012-07-29 08:15:41 -0700

    update version number

--- src/header.js
+++ src/header.js
@@ -1,5 +1,5 @@
 /*
-Maria release candidate 1 - an MVC framework for JavaScript applications
+Maria release candidate 2 - an MVC framework for JavaScript applications
 Copyright (c) 2012, Peter Michaux
 All rights reserved.
 Licensed under the Simplified BSD License.
