ab7b8ad7cef5755163ba7a794ebe7790364ea6f0
Peter Michaux <petermichaux@gmail.com>
2012-07-29 09:32:52 -0700

    add the actual util.js file

---
+++ eg/checkit/src/js/util.js
@@ -0,0 +1,11 @@
+checkit.trim = function(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+};
+
+checkit.isBlank = function(str) {
+    return /^\s*$/.test(str);
+};
+
+checkit.escapeHTML = function(str) {
+    return str.replace('&', '&amp;').replace('<', '&lt;');
+};
