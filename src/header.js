1a8c3151edc59d6082faa7bb48fc6e0c4532f959
Peter Michaux <petermichaux@gmail.com>
2013-04-08 22:20:34 -0700

    make header more Closure Compiler friendly

--- src/header.js
+++ src/header.js
@@ -1,7 +1,9 @@
-/*
-Maria release candidate 5 - an MVC framework for JavaScript applications
-Copyright (c) 2012, Peter Michaux
+/**
+@license
+Maria release candidate 6 - an MVC framework for JavaScript applications
+Copyright (c) 2013, Peter Michaux
 All rights reserved.
 Licensed under the Simplified BSD License.
 https://github.com/petermichaux/maria/blob/master/LICENSE
+
 */
