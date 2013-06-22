2da2b309a39e77d7ec80884ab76f539c04201751
Peter Michaux <petermichaux@gmail.com>
2013-06-22 12:08:06 -0700

    whitespace

--- src/namespace.js
+++ src/namespace.js
@@ -8,12 +8,9 @@ Root namespace
 var maria = {};
 
 /* DEBUG BEGIN */
-
 // Help older browsers without the `console` host object.
-
 var console = console || {};
 console.log = console.log || function() {};
 console.warn = console.warn || function() {};
 console.error = console.error || function() {};
-
 /* DEBUG END */
