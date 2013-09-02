430860d619c3d7020c1405e698ae682b7be93771
Peter Michaux <petermichaux@gmail.com>
2013-09-02 12:37:04 -0700

    whitespace cleanup

--- src/borrowGrail.js
+++ src/borrowGrail.js
@@ -5,17 +5,13 @@ Find an element matching a query.
 See grail.find for description.
 
 */
-
 maria.find = grail.find;
 
-/** 
+/**
 
 Find all elements matching a query.
 
 See grail.findAll for description.
 
 */
-
 maria.findAll = grail.findAll;
-
-
