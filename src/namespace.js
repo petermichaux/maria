c8266ad17a724e53af1cda0e98a64a53e3919c21
Ryan Scott <ryan.scott@tickbox.com.au>
2014-02-20 10:41:32 +1100

    Fix a bug where maria wasn't using the window console for debugging. It was instead trying to reference an undefined console variable scoped within the maria IIFE.

--- src/namespace.js
+++ src/namespace.js
@@ -9,7 +9,7 @@ var maria = {};
 
 /* DEBUG BEGIN */
 // Help older browsers without the `console` host object.
-var console = console || {};
+var console = window.console || {};
 console.log = console.log || function() {};
 console.warn = console.warn || function() {};
 console.error = console.error || function() {};
