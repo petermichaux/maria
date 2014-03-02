9f81f495dd16a9c352793001cd652d30916a69eb
Ryan Scott <ryan.scott@tickbox.com.au>
2014-03-03 10:15:49 +1100

    Revert "Fix a bug where maria wasn't using the window console for debugging."
    This reverts commit c8266ad17a724e53af1cda0e98a64a53e3919c21.

--- src/namespace.js
+++ src/namespace.js
@@ -9,7 +9,7 @@ var maria = {};
 
 /* DEBUG BEGIN */
 // Help older browsers without the `console` host object.
-var console = window.console || {};
+var console = console || {};
 console.log = console.log || function() {};
 console.warn = console.warn || function() {};
 console.error = console.error || function() {};
