c1ec635229494a8ac2624e4460a7deeaade6017e
Peter Michaux <petermichaux@gmail.com>
2016-01-23 21:30:10 -0800

    update header in anticipaiton of a release

--- src/header.js
+++ src/header.js
@@ -1,8 +1,8 @@
 /**
 @license
-Maria 1.1.0
-Copyright (c) 2013, Peter Michaux
+Maria 1.2.0
+Copyright (c) 2016, Peter Michaux
 All rights reserved.
 Licensed under the Simplified BSD License.
-http://peter.michaux.ca/downloads/maria/1.1.0/LICENSE
+http://peter.michaux.ca/downloads/maria/1.2.0/LICENSE
 */
